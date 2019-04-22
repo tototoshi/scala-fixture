3442ca2b115f279b299d0ab87d1d2fa635204172
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2019-04-22 11:44:03 +0900

    Remove webjars (#8)
    * Updated Play version
    
    * Removed webjars

--- build.sbt
+++ build.sbt
@@ -62,9 +62,7 @@ lazy val play = Project(
 ).enablePlugins(SbtTwirl).settings(
   name := "scala-fixture-play",
   libraryDependencies ++= Seq(
-    "com.typesafe.play" %% "play" % "2.6.3" % "provided",
-    "org.webjars" % "webjars-locator" % "0.32-1",
-    "org.webjars" % "bootstrap" % "3.3.7-1"
+    "com.typesafe.play" %% "play" % "2.7.1" % "provided"
   )
 ).settings(commonSettings ++ publishSettings).dependsOn(core)
 
@@ -76,12 +74,10 @@ lazy val playapp = Project(
   routesGenerator := InjectedRoutesGenerator,
   libraryDependencies ++= Seq(
     guice,
-    "org.flywaydb" %% "flyway-play" % "4.0.0",
+    "org.flywaydb" %% "flyway-play" % "5.3.2",
     jdbc,
     "org.scalikejdbc" %% "scalikejdbc" % "3.0.2",
     "org.scalikejdbc" %% "scalikejdbc-config" % "3.0.2",
-    "org.webjars" %% "webjars-play" % "2.6.1",
-    "org.webjars" % "bootstrap" % "3.3.7-1",
     "com.h2database" % "h2" % "1.4.+",
     "org.scalatest" %% "scalatest" % "3.0.3" % "test"
   )
--- play/src/main/scala/com/github/tototoshi/fixture/play/FixtureAdminWebCommand.scala
+++ play/src/main/scala/com/github/tototoshi/fixture/play/FixtureAdminWebCommand.scala
@@ -1,19 +1,14 @@
 package com.github.tototoshi.fixture.play
 
-import java.io.{ ByteArrayOutputStream, File, InputStream }
-import java.util.regex.Pattern
-import javax.inject.{ Inject, Singleton }
+import java.io.File
 
-import com.github.tototoshi.fixture.IOUtil
-import org.webjars.WebJarAssetLocator
+import javax.inject.{ Inject, Singleton }
 import play.api.mvc.Results._
 import play.api.mvc.{ RequestHeader, Result }
 import play.api.{ Environment, Mode }
 import play.core.{ BuildLink, HandleWebCommandSupport, WebCommands }
 
 class FixtureWebCommandHandler(environment: Environment, fixtures: Fixtures) extends HandleWebCommandSupport {
-
-  private val webJarAssetLocator = new WebJarAssetLocator(WebJarAssetLocator.getFullPathIndex(Pattern.compile(".*"), environment.classLoader))
 
   private object Path {
     def unapplySeq(s: String): Option[Seq[String]] =
@@ -41,31 +36,12 @@ class FixtureWebCommandHandler(environment: Environment, fixtures: Fixtures) ext
         case Path("@fixture", dbName, "tearDown") =>
           fixtures.get(dbName).foreach(_.tearDown())
           Some(Redirect(s"/@fixture/${dbName}"))
-        case Path("@fixture", "assets", assets @ _*) =>
-          Option(environment.classLoader.getResource(webJarAssetLocator.getFullPath(assets.mkString("/")))).map {
-            resource =>
-              IOUtil.using(resource.openStream()) { stream =>
-                Some(Ok(readInputStreamToString(stream)))
-              }
-          }.getOrElse(Some(NotFound))
         case _ => None
       }
     }
   }
 
   private def isDev(environment: Environment): Boolean = environment.mode == Mode.Dev
-
-  private def readInputStreamToString(stream: InputStream): String = {
-    val buffer = new Array[Byte](8192)
-    var len = stream.read(buffer)
-    IOUtil.using(new ByteArrayOutputStream()) { out =>
-      while (len != -1) {
-        out.write(buffer, 0, len)
-        len = stream.read(buffer)
-      }
-      new String(out.toByteArray)
-    }
-  }
 
 }
 
